162af08929e24ac384c119ee827e96c40c3cff93
ranranluu <ranranluutennis@gmail.com>
2021-06-13 10:30:56 +0000

    [update]form_withが使えない理由はrailsのアップデートができていなかったから

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about', to: 'homes#about'
   resources :books, only: [:new, :create, :index, :show, :destroy]
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:show, :edit, :update]
 end
