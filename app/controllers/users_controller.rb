ba97c3c2dadac7d96766cb7e3f8ec5c3a5b83611
ranranluu <ranranluutennis@gmail.com>
2021-06-13 09:10:55 +0000

    [update]プロフィール編集画面まで一応進んだ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+   private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
---
+++ db/migrate/20210613075101_addimage_id_to_users.rb
@@ -0,0 +1,4 @@
+class AddimageIdToUsers < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
