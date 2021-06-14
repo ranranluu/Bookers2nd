35f2ba1dd9a2c2169deff922adfe477c2226db9d
ranranluu <ranranluutennis@gmail.com>
2021-06-14 16:52:46 +0000

    [update]ひとまず完成、テストやってみる

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  
+  before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -10,9 +10,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
     devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
-  
+
+  def afterr_sign_out_path_for(resource)
+    root_path
+  end
+
   def after_sign_in_path_for(resource)
     user_path(resource)
   end
-  
+
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,6 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice]="You have created book successfully."
       redirect_to book_path(@book)
     else
       @user = current_user
@@ -19,6 +20,7 @@ class BooksController < ApplicationController
   def index
     @user = current_user
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -35,6 +37,7 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @book.user_id = current_user.id
     if @book.update(book_params)
+      flash[:notice]="You have updated book successfully."
       redirect_to book_path(@book.id)
     else
       render :edit
