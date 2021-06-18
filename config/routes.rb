8b99f3f1f84859238f655e6aec00b000f09ca6df
ranranluu <ranranluutennis@gmail.com>
2021-06-18 07:53:48 +0000

    [fix]少しだけ見やすく

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-
   devise_for :users
   root to: 'homes#top'
   get '/home/about', to: 'homes#about'
   
   resources :books, only: [:update, :create, :index, :show, :destroy, :edit]
   resources :users, only: [:show, :edit, :update, :index]
+  
 end
