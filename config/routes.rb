07d232329625a628c17cf4e54b317d2fea79fca7
ranranluu <ranranluutennis@gmail.com>
2021-07-06 07:46:03 +0000

    [update]課題５OK、課題６に進む

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
 
+
   devise_for :users
   root to: 'homes#top'
   get '/home/about', to: 'homes#about'
+  get '/search' => 'search#search'
+
 
   resources :books, only: [:update, :create, :index, :show, :destroy, :edit] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users, only: [:show, :edit, :update, :index] do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
