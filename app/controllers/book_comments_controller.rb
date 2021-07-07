36fc5337f910572b0f53277e3d782deee7bdd314
ranranluu <ranranluutennis@gmail.com>
2021-07-07 14:20:38 +0000

    [question]コメント追加が何故かjsにならない、明日質問する

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,17 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    comment = BookComment.find(params[:id])
-    comment.book_id = book.id
-    comment.destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comment = BookComment.find(params[:id])
+    @book_comment.book_id = @book.id
+    @book_comment.destroy
   end
 
   private
